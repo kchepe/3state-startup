0af809fe81824c77f59cbb7820ccd03cc50c13ce
Bryan Libod-Libod <bryan@invedent.com>
2023-08-20 21:06:37 +0800

    update nextjs version

--- client/next.config.js
+++ client/next.config.js
@@ -8,11 +8,6 @@ const nextConfig = {
     GRAPHQL_ENDPOINT: 'http://server:3000/graphql',
     ENVIRONMENT: 'development',
   },
-  modularizeImports: {
-    'react-icons': {
-      transform: 'react-icons/{{member}}',
-    },
-  },
 };
 
 module.exports = nextConfig;
