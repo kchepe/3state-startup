41ca6aeb46679e8e8a575897a38cb10deff8b9a2
Bryan Libod-Libod <bryan@invedent.com>
2023-08-20 15:37:35 +0800

    remove unused page

--- client/src/app/(pages)/(main)/(private)/(pages)/my-properties/[id]/page.tsx
+++ client/src/app/(pages)/(main)/(private)/(pages)/my-properties/[id]/page.tsx
@@ -1,5 +1,20 @@
+'use client';
+
 import React from 'react';
+import DashboardWrapper from '@/app/common/DashboardWrapper';
+import Form from '@/app/common/FormBuilder/Form';
+import PropertyForm from './components/PropertyForm';
+import {
+  addPropertyInitialValues,
+  addPropertySchema,
+} from './components/PropertyForm/propertyFormValues';
 
-const PropertyForm = () => <div>page</div>;
+const AddProperty = () => (
+  <DashboardWrapper title="Add Property">
+    <Form initialValues={addPropertyInitialValues} schema={addPropertySchema}>
+      <PropertyForm />
+    </Form>
+  </DashboardWrapper>
+);
 
-export default PropertyForm;
+export default AddProperty;
