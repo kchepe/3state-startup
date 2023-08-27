ded0b4b5924de4ec74f2dec8a1054f7023c2698b
Bryan Libod-Libod <bryan@invedent.com>
2023-08-27 21:34:40 +0800

    added search icon

--- client/src/app/(pages)/(main)/(public)/property/[type]/(propertyList)/Components/PropertyList/Components/SearchField/index.tsx
+++ client/src/app/(pages)/(main)/(public)/property/[type]/(propertyList)/Components/PropertyList/Components/SearchField/index.tsx
@@ -6,6 +6,7 @@ import Toggle from '@/app/common/Toggle';
 import usePropertyManager from '@/app/hooks/usePropertyManager';
 import Box from '@/app/common/Box';
 import Text from '@/app/common/Text';
+import Search from '@/app/icons/Search';
 import { properties } from '../../mockData';
 
 interface SearchFieldProps {
@@ -32,7 +33,7 @@ const SearchField: FC<SearchFieldProps> = ({ currentWidth }) => {
           placeholder="Search property here..."
           className="p-3 bg-gray-100"
           type="search"
-          // startIcon={<BiSearch className="text-gray-500 text-lg" />}
+          startIcon={<Search className="text-gray-500 w-4 h-4" />}
         />
       </Box>
       <Box className="flex items-center justify-between mt-2">
