3fa5cf369fef1c97e2966c588d83acf195c6d579
Bryan Libod-Libod <bryan@invedent.com>
2023-11-19 21:12:05 +0800

    adjusted style

--- client/src/app/(pages)/(main)/(public)/property/[type]/(propertyList)/Components/PropertyList/index.tsx
+++ client/src/app/(pages)/(main)/(public)/property/[type]/(propertyList)/Components/PropertyList/index.tsx
@@ -5,6 +5,7 @@ import clsx from 'clsx';
 import Box from '@/app/common/Box';
 import PropertyCard from '@/app/common/PropertyCard';
 import { IProperty } from '@/app/types/types';
+import usePropertyManager from '@/app/hooks/usePropertyManager';
 import SearchField from './Components/SearchField';
 
 interface PropertyListProps {
@@ -15,23 +16,34 @@ interface PropertyListProps {
   propertyLoading: boolean;
 }
 
-const PropertyList = ({ properties, propertyLoading }: PropertyListProps) => (
-  <Box className="grid grid-cols-12 gap-5 relative">
-    <Box className="col-span-full">
-      <SearchField totalPropertyCount={properties?.totalCount ?? 0} />
+const PropertyList = ({ properties, propertyLoading }: PropertyListProps) => {
+  const {
+    propertiesState: {
+      filter: { showMap },
+    },
+  } = usePropertyManager();
+  return (
+    <Box
+      className={clsx('grid grid-cols-12 gap-5', {
+        'pr-4': !showMap,
+      })}
+    >
+      <Box className="col-span-full">
+        <SearchField totalPropertyCount={properties?.totalCount ?? 0} />
+      </Box>
+      <Box className={clsx('col-span-full grid grid-cols-card-list gap-8')}>
+        {propertyLoading ? (
+          <div>Loading...</div>
+        ) : (
+          properties.properties.map((property: IProperty) => (
+            <Box key={property.id} className="col-span-1">
+              <PropertyCard property={property} />
+            </Box>
+          ))
+        )}
+      </Box>
     </Box>
-    <Box className={clsx('col-span-full grid grid-cols-card-list gap-8')}>
-      {propertyLoading ? (
-        <div>Loading...</div>
-      ) : (
-        properties.properties.map((property: IProperty) => (
-          <Box key={property.id} className="col-span-1">
-            <PropertyCard property={property} />
-          </Box>
-        ))
-      )}
-    </Box>
-  </Box>
-);
+  );
+};
 
 export default PropertyList;
